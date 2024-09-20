ea1a336e5f28624cbd2ce85ba8bc849be200beb0
Nikolett Lisoczki <privat.princess.chaos@gmail.com>
2024-09-20 19:31:33 +0200

    initial project

---
+++ LnWebshopReact/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+  return (
+    <div className='app'>
+      <nav>Navbar</nav>
+      <main>All routing</main>
+
+    </div>
+  )
+}
+
+export default App
